7d317acb0d36eb26f66e07dcb3c783e700c023f1
maman510 <153794692+maman510@users.noreply.github.com>
2024-11-18 03:54:54 -0800

    Add files via upload

--- custom_vae.py
+++ custom_vae.py
@@ -8,6 +8,13 @@ import tool_box
 from tabulate import tabulate
 from custom_loss import VAELoss, KLDivergenceMetric, ReconstructionLossMetric
 from keras.datasets import cifar10
+import random
+import numpy as np
+
+# Set random seeds for reproducibility
+random.seed(42)  # For Python's built-in random module
+np.random.seed(42)  # For NumPy
+tf.random.set_seed(42)  # For TensorFlow
 
 
 class VAE(keras.Model):
@@ -57,111 +64,93 @@ class VAE(keras.Model):
         self.summary_table = tabulate(table, headers, tablefmt="fancy_grid")
         print(f"\n\n{self.summary_table}\n\n")
 
-    def _check_loss(self, epochs, logs):
-        'callback mimics EarlyStop by checking min_delta and patience against current and past epoch data - avoids restarting while allowing update to weights'
-        if self.current_val_loss == None:
-            self.current_val_loss = logs["loss"]
-            display = f"\n\nIN CHECK LOSSES - INITIAL LOSS\n\n"
-            print(tool_box.color_string('cyan', display))
-            return self._update_losses(logs, update_weights=False)
+     
+    def train_step(self, data):
+            # Unpack the data
+            x, y = data
+
+            with tf.GradientTape() as tape:
+                # Forward pass
+                y_pred = self(x)
+
+                # Compute the loss
+                loss = self.vae_loss(y, y_pred)
+ 
+
+            # Check that the loss is a valid scalar
+            tf.debugging.check_numerics(loss, "Loss contains NaN or Inf")
+
+            # Get gradients
+            grads = tape.gradient(loss, self.trainable_variables)
+
+            # Apply gradients
+            self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+            return {'loss': loss}
+
+
+    def _check_loss(self, epoch, logs):
+        kl_loss = self.current_kl_loss.result() 
+        recon_loss = self.current_reconstruction_loss.result()
+        # Decide whether to adjust the weights based on observed losses
+        if kl_loss > 2 * recon_loss:
+            # If KL loss is much larger than reconstruction loss, reduce kl_beta
+            print(tool_box.color_string('red',f"\n\nFinal Epoch KL_LOSS (no weight): {kl_loss} Recon Loss (no weight): {recon_loss}; KL_loss weighted: {kl_loss * self.kl_beta}\tRecon Loss weighted: {recon_loss * self.reconstruction_weight}"))
+            print(tool_box.color_string('green', f"\nKL Loss much higher than recons loss\tReducing Current KL Beta:\t{self.kl_beta}"))
+            self.kl_beta *= 0.9  # Decrease the influence of KL loss
+            print(tool_box.color_string('green', f"\nNew KL Beta:\t{self.kl_beta}\n"))
+
+           
+        elif recon_loss > 2 * kl_loss:
+            # If reconstruction loss is much larger, increase reconstruction weight
+            print(tool_box.color_string('red',f"\n\nFinal Epoch KL_LOSS (no weight): {kl_loss} Recon Loss (no weight): {recon_loss}; KL_loss weighted: {kl_loss * self.kl_beta}\tRecon Loss weighted: {recon_loss * self.reconstruction_weight}"))
+            print(tool_box.color_string('green', f"\nRecons loss much highter than kl loss\tIncreasing Reconstruction Weight:\t{self.reconstruction_weight}"))
+            self.reconstruction_weight *= 1.1  # Increase the influence of reconstruction loss
+            print(tool_box.color_string('green', f"\nNew Reconstruction Weight:\t{self.reconstruction_weight}"))
         else:
-            epoch_val_loss = logs["loss"]
-            delta = self.current_val_loss - epoch_val_loss
-            print(tool_box.color_string('white', f"\n\nCURRENT_VAL_LOSS: {epoch_val_loss}; PREVIOUS VAL_LOSS: {self.current_val_loss}; DELTA: {delta}; min_delta: {self.min_delta}\n\n"))
-            if delta < self.min_delta:
-                #handle min_delta and patience compromise
-
-                if self.current_patience_count >= self.patience:
-                    #handle update weights here
-                    display = f"\n\nIN CHECK LOSSES - TRIGGERING WEIGHT UPDATE\n\n"
-                    self.current_patience_count = 0
-                    self.current_val_loss = epoch_val_loss
-                    print(tool_box.color_string('red', display))
-                    return self._update_losses(logs, update_weights=True)
-                else:
-                    #handle incrementing patience only
-                    self.current_val_loss = epoch_val_loss
-                    self.current_patience_count += 1
-                    display = f"\n\nIN CHECK LOSSES - MIN DELTA THRESHOLD MET; UPDATING PATIENCE COUNT: {self.current_patience_count}\n"
-                    print(tool_box.color_string('yellow', display))
-                    return self._update_losses(logs, update_weights=False)
-            else:
-                #handle regular epoch output (no min_delta)
-                print(tool_box.color_string('green', f"\n\nIN _UPDATE_LOSSES - ADDING NEW LOSSES FROM EPOCH...\n\n"))
-                return self._update_losses(logs, update_weights=False)
-
-    def _update_losses(self, logs, update_weights):
-        '''method called by self._check_losses - updates aggregated losses and update kl_beta and recon_weight if needed'''
-        total_loss = logs["loss"]
-        kl_loss = logs["kl_divergence"]
-        recon_loss = logs["reconstruction_loss"]
-
-        if update_weights == False:
-            #scale losses and add to aggregate here
-            self.current_total_losses.append(total_loss)
-            self.current_kl_val_losses.append(kl_loss)
-            self.current_reconstruction_val_losses.append(recon_loss)
-            display = f"\n\nIN _UPDATE_LOSSES - LOSSES UPDATED\n\nKL: {self.current_kl_val_losses}\tRECON: {self.current_reconstruction_val_losses}\tTOTAL: {self.current_total_losses}\n\n"
-            print(tool_box.color_string('yellow', display))
-            return None
-        else:
-            #handle update here
-            display = f"\n\nIN _UPDATE_LOSSES; MIN_DELTA AND PATIENCE THRESHOLD MET....UPDATING WEIGHTS...."
-            self.current_total_losses.append(total_loss)
-            self.current_kl_val_losses.append(kl_loss)
-            self.current_reconstruction_val_losses.append(recon_loss)
-            print(tool_box.color_string('cyan', display))
-            return None
-            #check sum(self.current_total_loss) against sum(kl_loss) and sum(recon_loss) to see proportions; NOTE: losses are scaled so comparison
-
-
-    def run_training(self, x_train, x_test, optimizer,  epochs, kl_beta=1, learning_rate=None, batch_size=1, reshape_dims=None, callbacks=None):
-        #normalize train and test data
-        x_train = x_train.astype("float32") / 255.0
-        x_test = x_test.astype("float32") / 255.0
-        if reshape_dims != None:
-            print(tool_box.color_string("yellow", f"\nRESHAPING INPUT DATA FROM {x_train.shape[1:]}\tTO\t{(*reshape_dims,3)}....\n"))
-            x_train = tf.image.resize(x_train, reshape_dims, method="lanczos5")
-            x_train = tf.cast(x_train, dtype=tf.float32)
-            x_test = tf.image.resize(x_test, reshape_dims, method="lanczos5")
-            x_test = tf.cast(x_test, dtype=tf.float32)
-            
-        #set input_shape and build model
-        input_shape = x_train.shape[1:]
-        self.build((None, *input_shape))
-
-        if optimizer == "adam":
-            if learning_rate == None:
-                optimizer = keras.optimizers.Adam()
-            else:
-                optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-
-        #initiate loss params
-        kl_beta = 1.0
-        reconstruction_weight = 1.0
-        # self.current_kl_val_loss = None
-        # self.current_reconstruction_val_loss = None
-
-        self.min_delta = 0.1
-        self.patience = 5
-        self.current_val_loss = None
-        self.current_patience_count = 0
-
-        self.current_total_losses = []
-        self.current_kl_val_losses = []
-        self.current_reconstruction_val_losses = []
-
-
-
-        self.vae_loss = VAELoss(encoder=self.encoder, kl_beta=kl_beta, reconstruction_weight=reconstruction_weight)
-        self.compile(optimizer=optimizer, loss=self.vae_loss, metrics=[ReconstructionLossMetric(), KLDivergenceMetric(beta=kl_beta)])
+            print(tool_box.color_string('red',f"\n\nFinal Epoch KL_LOSS (no weight): {kl_loss} Recon Loss (no weight): {recon_loss}; KL_loss weighted: {kl_loss * self.kl_beta}\tRecon Loss weighted: {recon_loss * self.reconstruction_weight}"))
         
-        self.summary()
 
-        cb = [
-            keras.callbacks.LambdaCallback(on_epoch_end=lambda epochs, logs: self._check_loss(epochs, logs)),
-            ]
-        return self.fit(x_train, x_train,batch_size=batch_size, epochs=epochs, validation_data=(x_test, x_test), callbacks=cb)
+
+
+    def run_training(self, x_train, x_test, optimizer,  epochs, learning_rate=None, batch_size=1, reshape_dims=None, callbacks=None):
+            #normalize train and test data
+            x_train = x_train.astype("float32") / 255.0
+            x_test = x_test.astype("float32") / 255.0
+            if reshape_dims != None:
+                print(tool_box.color_string("yellow", f"\nRESHAPING INPUT DATA FROM {x_train.shape[1:]}\tTO\t{(*reshape_dims,3)}....\n"))
+                x_train = tf.image.resize(x_train, reshape_dims, method="lanczos5")
+                x_train = tf.cast(x_train, dtype=tf.float32)
+                x_test = tf.image.resize(x_test, reshape_dims, method="lanczos5")
+                x_test = tf.cast(x_test, dtype=tf.float32)
+                
+            #set input_shape and build model
+            input_shape = x_train.shape[1:]
+            self.build((None, *input_shape))
+
+            if optimizer == "adam":
+                if learning_rate == None:
+                    optimizer = keras.optimizers.Adam()
+                else:
+                    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+
+            #initiate loss params
+            self.kl_beta = 1.0
+            self.reconstruction_weight = 1.0
+            self.vae_loss = VAELoss(encoder=self.encoder, kl_beta=self.kl_beta, reconstruction_weight=self.reconstruction_weight)
+                       # self.current_kl_val_loss = None
+
+            self.current_reconstruction_loss = ReconstructionLossMetric()
+            self.current_kl_loss = KLDivergenceMetric(beta=self.kl_beta)
+
+            self.compile(optimizer=optimizer, loss=self.vae_loss, metrics=[self.current_reconstruction_loss, self.current_kl_loss])
+            
+            self.summary()
+
+            cb = [
+                keras.callbacks.LambdaCallback(on_epoch_end=lambda epochs, logs: self._check_loss(epochs, logs)),
+                ]
+            return self.fit(x_train, x_train,batch_size=batch_size, epochs=epochs, validation_data=(x_test, x_test), callbacks=cb, verbose=0)
 
 
 
@@ -171,20 +160,17 @@ latent_dims = 2
 vae = VAE(latent_dims=latent_dims)
 batch_size = 2056
 reshape_dims = (64,64)
-kl_beta = 0.1
-epochs = 500
-cb = [keras.callbacks.EarlyStopping(monitor="loss", min_delta=0.0001, patience=5, mode="min")]
+
+epochs = 5000
+
 vae.run_training(x_train=x_train, 
-                 x_test=x_test,
-                 optimizer="adam", 
-                 epochs=epochs, 
-                 kl_beta=kl_beta, 
-                 learning_rate=0.001, 
-                 batch_size=56,
-                 callbacks=cb,
+                x_test=x_test,
+                optimizer="adam", 
+                epochs=epochs,
+                learning_rate=0.0001, 
+                batch_size=batch_size,
+                # callbacks=cb,
                 #  reshape_dims=reshape_dims
-                )
-
-
+)
 
 
