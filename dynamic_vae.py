9ed37665544f31ee2b5b6346d701649580fa3aeb
maman510 <153794692+maman510@users.noreply.github.com>
2024-11-20 18:01:54 -0800

    Add files via upload

--- dynamic_vae.py
+++ dynamic_vae.py
@@ -1,4 +1,6 @@
 import keras
+import keras.applications
+import keras.applications.vgg16
 import keras.layers
 import keras.losses
 import keras.optimizers
@@ -19,11 +21,53 @@ class DynamicVAE(keras.Model):
         self.encoder = encoder
         self.decoder = decoder
         self.kl_beta = kl_beta_initial
+        
+        #=== vgg and ssim
+        self.vgg_model = self._build_vgg_model()
+
+        
+        
         #define total epochs for training
         self.training_epoch_count = training_epoch_count
         self.current_epoch = 0
         self.current_recon_level = None
+        
+    def _build_vgg_model(self):
+        vgg = keras.applications.VGG16(weights="imagenet", include_top=False)
+      #  'block1_conv2', 'block2_conv2' ,'block5_conv2', 'block5_conv3'
+        layer_names =    ['block3_conv2', 'block3_conv3', 'block4_conv2','block4_conv3']
+        vgg_outputs = [vgg.get_layer(name).output for name in layer_names]
+        vgg_model = keras.Model(inputs=vgg.input, outputs=vgg_outputs)
+        vgg_model.trainable = True
+        return vgg_model
+ 
+    def _vgg_loss(self, original, reconstructed):
+        original_preprocessed = keras.applications.vgg16.preprocess_input(original * 255.0)
+        reconstructed_preprocessed = keras.applications.vgg16.preprocess_input(reconstructed * 255.0)
+        original_features = self.vgg_model(original_preprocessed)
+        reconstructed_features = self.vgg_model(reconstructed_preprocessed)
+        loss = 0
+        for original_feat, recon_feat in zip(original_features, reconstructed_features):
+            loss += tf.reduce_mean(tf.square(original_feat -  recon_feat))
+        
+        return loss
 
+    def _ssim_loss(self, original, reconstructed):
+        return 1 - tf.reduce_mean(tf.image.ssim(original, reconstructed, max_val=1.0))
+
+    def combined_loss(self, original, reconstructed, z_mean, z_log_var):
+        mse_loss = tf.reduce_mean(tf.square(original - reconstructed))
+        perceptual_loss = self._vgg_loss(original, reconstructed)
+        ssim_loss = self._ssim_loss(original, reconstructed)
+        kl_loss = self.compute_kl_divergence(z_mean, z_log_var)
+
+        #set weights for reconstruction, vgg, and ssim losses
+        alpha = 1.0 #for reconstruction loss
+        beta = 0.1 #for vgg_loss
+        gamma = 0.1 #for ssim_loss
+
+        total_loss = (alpha * mse_loss) + (beta * perceptual_loss) + (gamma * ssim_loss) + kl_loss
+        return kl_loss, mse_loss, perceptual_loss, ssim_loss, total_loss
     
     def call(self, inputs):
         z_mean, z_log_var = self.encoder(inputs)
@@ -42,30 +86,31 @@ class DynamicVAE(keras.Model):
                         tf.exp(z_log_var), axis=-1)
         return tf.reduce_mean(kl_div)
     
-    def compute_reconstruction_error(self, inputs, reconstructed):
-        return tf.reduce_mean(tf.square(inputs - reconstructed))
+    def compute_reconstruction_error(self, inputs, reconstructed, delta=1.0):
+        huber_loss = tf.keras.losses.Huber(delta=delta)
+        return huber_loss(inputs, reconstructed)
     
 
-    def train_step(self, data):
+    def train_step(self, original_images):
         self.current_epoch += 1
         with tf.GradientTape() as tape:
             #forward pass:
-            reconstructed, z_mean, z_log_var = self(data)
+            reconstructed, z_mean, z_log_var = self(original_images)
 
             #calculate kl_div and recon error
-            kl_divergence = self.compute_kl_divergence(z_mean, z_log_var)
-            reconstruction_error = self.compute_reconstruction_error(data, reconstructed)
-
+           # kl_divergence = self.compute_kl_divergence(z_mean, z_log_var)
+           # reconstruction_error = self.compute_reconstruction_error(original_images, reconstructed)
+            kl_divergence, mse_loss, perceptual_loss, ssim_loss, total_loss = self.combined_loss(original_images, reconstructed, z_mean, z_log_var)
             #adjust kl_beta
-            self.kl_beta = self.adjust_kl_beta(reconstruction_error, kl_divergence)
-            total_loss = reconstruction_error + (kl_divergence * self.kl_beta)
+           # self.kl_beta = self.adjust_kl_beta(reconstruction_error, kl_divergence)
+           # total_loss = reconstruction_error + (kl_divergence * self.kl_beta)
 
         gradients = tape.gradient(total_loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
 
      
 
-        return {"loss": total_loss, "kl_divergence": kl_divergence, "reconstruction_error": reconstruction_error, "kl_beta": self.kl_beta}
+        return {"loss": total_loss, "vgg_loss": perceptual_loss , "ssim_loss": ssim_loss, "reconstruction_error": mse_loss, "kl_divergence": kl_divergence}
     
 
     def _check_update(self, recon_error):
@@ -103,7 +148,29 @@ class DynamicVAE(keras.Model):
                 }
         
         return beta_schedule[self.current_recon_level]
-        
+    
+    def reconstruct_image(self, original_image):
+        # Reconstruct the image using the VAE
+        reconstructed_image = vae.predict(original_image)
+
+        # The model returns a tuple (outputs, loss) from the VAE. We need to get the output (reconstructed image).
+        reconstructed_image = reconstructed_image[0]  # This extracts the first element of the tuple, which is the reconstructed image.
+
+        # Display the original and reconstructed image side by side
+        fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+
+        # Original Image
+        axes[0].imshow(original_image.squeeze())
+        axes[0].set_title("Original Image")
+        axes[0].axis("off")
+
+        # Reconstructed Image
+        axes[1].imshow(reconstructed_image.squeeze())
+        axes[1].set_title("Reconstructed Image")
+        axes[1].axis("off")
+
+        plt.show()
+                
 
 
 
@@ -141,12 +208,13 @@ x_train = x_train.astype("float32")/255.0
 x_test = x_test.astype("float32")/255.0
 
 input_shape = (32,32,3)
-latent_dim = 2
+
+latent_dim = 32
 
 encoder = build_encoder(input_shape, latent_dim)
 decoder = build_decoder(latent_dim, input_shape)
 
-epochs=10
+epochs=200
 
 vae = DynamicVAE(encoder, decoder, training_epoch_count=epochs)
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
@@ -161,32 +229,9 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 vae.compile(optimizer=optimizer)
 
 
-vae.fit(x_train,  epochs=epochs, batch_size=64)
-
-        
-# kl_beta_schedule = tf.keras.callbacks.LearningRateScheduler(lambda epoch: 0.1 * (epoch + 1))
+vae.fit(x_train[:100],  epochs=epochs, batch_size=32)
 
 # Testing with one image
 original_image = x_test[0:1]  # Take the first image from the test set
 
-
-# Reconstruct the image using the VAE
-reconstructed_image = vae.predict(original_image)
-
-# The model returns a tuple (outputs, loss) from the VAE. We need to get the output (reconstructed image).
-reconstructed_image = reconstructed_image[0]  # This extracts the first element of the tuple, which is the reconstructed image.
-
-# Display the original and reconstructed image side by side
-fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-
-# Original Image
-axes[0].imshow(original_image.squeeze())
-axes[0].set_title("Original Image")
-axes[0].axis("off")
-
-# Reconstructed Image
-axes[1].imshow(reconstructed_image.squeeze())
-axes[1].set_title("Reconstructed Image")
-axes[1].axis("off")
-
-plt.show()
+vae.reconstruct_image(original_image)
