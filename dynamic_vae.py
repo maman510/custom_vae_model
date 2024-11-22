13db6bd65c80e3a50a27311800a0baf2f00c02e3
maman510 <153794692+maman510@users.noreply.github.com>
2024-11-22 05:06:12 -0800

    Add files via upload

--- dynamic_vae.py
+++ dynamic_vae.py
@@ -21,8 +21,8 @@ tf.config.run_functions_eagerly(True)
 class DynamicVAE(keras.Model):
     '''
         Custom VAE model that adjusts the following weights during training (based on schedule):
-            - Alpha (vgg weight)
-            - Gamma (ssim weight)
+            - recon_weight (vgg weight)
+            - vgg_weight (ssim weight)
             - Beta (kl divergence weight)
 
     '''
@@ -31,7 +31,16 @@ class DynamicVAE(keras.Model):
  
 #=============== Public Instance Methods ===================
 
-    def __init__(self, model_name, latent_dims, optimizer_fn, learning_rate, alpha=0.1, kl_beta=1.0, gamma=1.0):
+    def __init__(self, model_name, latent_dims, optimizer_fn, learning_rate, 
+                 recon_weight=1.0, 
+                 kl_beta=.01, 
+                 vgg_weight=0.1, 
+                 ssim_weight=1.0,
+                 recon_decay_rate=1.0,
+                 vgg_decay_rate = 0.95,
+                 kl_beta_decay_rate=0.95,
+                 ssim_decay_rate=1.0,
+                 ):
         super(DynamicVAE, self).__init__()
         self.model_name = model_name
         self.latent_dims = latent_dims
@@ -41,9 +50,17 @@ class DynamicVAE(keras.Model):
         self.learning_rate = learning_rate
         
         #set weights
-        self.alpha = alpha #for reconstruction loss
+        self.recon_weight = recon_weight #for reconstruction loss
         self.kl_beta = kl_beta #for vgg_loss
-        self.gamma = gamma  #for ssim_loss
+        self.vgg_weight = vgg_weight  #for ssim_loss
+        self.ssim_weight = ssim_weight
+
+
+        self.recon_decay_rate = recon_decay_rate
+        self.vgg_decay_rate = vgg_decay_rate
+        self.kl_beta_decay_rate = kl_beta_decay_rate
+        self.ssim_decay_rate = ssim_decay_rate
+        self.current_epoch = 0
 
         #compile
         self.compile(optimizer=self.optimizer_fn(self.learning_rate))
@@ -89,16 +106,15 @@ class DynamicVAE(keras.Model):
   
     def combined_loss(self, original, reconstructed, z_mean, z_log_var):
         reconstruction_loss = self._compute_reconstruction_error(original, reconstructed)
-        perceptual_loss = self._vgg_loss(original, reconstructed)
+        vgg_loss = self._vgg_loss(original, reconstructed)
         ssim_loss = self._ssim_loss(original, reconstructed)
-        kl_loss = self._compute_kl_divergence(z_mean, z_log_var)
+        kl_divergence = self._compute_kl_divergence(z_mean, z_log_var)
 
         #set weights for reconstruction, vgg, and ssim losses
-        total_loss = (self.alpha * reconstruction_loss) + (self.kl_beta * perceptual_loss) + (self.gamma * ssim_loss) + kl_loss
-        return kl_loss, reconstruction_loss, perceptual_loss, ssim_loss, total_loss
+        total_loss = (self.recon_weight * reconstruction_loss) + (self.vgg_weight * vgg_loss) + (self.ssim_weight * ssim_loss) + (kl_divergence * self.kl_beta)
+        return kl_divergence, reconstruction_loss,  vgg_loss, ssim_loss, total_loss
     
     def call(self, inputs):
-    
         z_mean, z_log_var = self.encoder(inputs)
         z = self._sampling(z_mean, z_log_var)
         reconstructed = self.decoder(z)
@@ -111,7 +127,7 @@ class DynamicVAE(keras.Model):
 
 
     def train_step(self, original_images):
-   
+        
         with tf.GradientTape() as tape:
             #forward pass:
             reconstructed, z_mean, z_log_var = self(original_images)
@@ -122,24 +138,30 @@ class DynamicVAE(keras.Model):
         gradients = tape.gradient(total_loss, self.trainable_variables)
         self.optimizer.apply_gradients(zip(gradients, self.trainable_variables))
  
- 
+        self.current_epoch += 1
 
         return {"loss": total_loss,
                 "reconstruction_error": reconstruction_loss, 
                 "kl_divergence": kl_divergence,
                 "vgg_loss": perceptual_loss, 
                 "ssim_loss": ssim_loss,
-                "alpha": self.alpha,
+                "recon_weight": self.recon_weight,
                 "kl_beta": self.kl_beta,
-                "gamma": self.gamma
+                "vgg_weight": self.vgg_weight,
+                "ssim_weight": self.ssim_weight
                 }
     
     
-    def scheduler_callback(self, epoch, logs, alpha_decay_rate, kl_beta_decay_rate, gamma_decay_rate):
-        # Update the values of alpha, kl_beta, gamma
-        self.alpha *= (1. + alpha_decay_rate)
-        self.kl_beta *= (1. - kl_beta_decay_rate)
-        self.gamma *= (1. - gamma_decay_rate)
+    def scheduler_callback(self, epoch, logs):
+        # Update the values of recon_weight, kl_beta, vgg_weight
+    
+        self.recon_weight *= self.recon_decay_rate
+        self.kl_beta *= self.kl_beta_decay_rate
+        self.vgg_weight *= self.vgg_decay_rate
+        self.ssim_weight *= self.ssim_decay_rate
+    
+    
+            #keep weights the same
 
     
     def reconstruct_image(self, original_image):
@@ -184,7 +206,20 @@ class DynamicVAE(keras.Model):
         
 
     def get_config(self):
-   
+        
+
+        #NOTE: IF NOT RESET WEIGHTS (i.e. - save latest decayed value after training), remove "initial" weights and uncomment custom configs keys
+
+        initial_recon_weight = 1.0 #recon loss
+        initial_kl_beta = .01   #kl_loss
+        initial_ssim_weight = 1.0 #ssim loss
+        initial_vgg_weight = 0.1
+
+        recon_decay_rate=1.0,
+        vgg_decay_rate = 0.95,
+        kl_beta_decay_rate=0.95,
+        ssim_decay_rate=0.95,
+
         custom_configs = {
             "model_name": self.model_name,
             "latent_dims": self.latent_dims,
@@ -195,9 +230,17 @@ class DynamicVAE(keras.Model):
             "optimizer_fn": self.optimizer_fn,
             "learning_rate": self.learning_rate,
             "weights": self.weights,
-            "alpha": self.alpha,
-            "kl_beta": self.kl_beta,
-            "gamma": self.gamma
+            "recon_weight": initial_recon_weight,
+            "kl_beta": initial_kl_beta,
+            "vgg_weight": initial_vgg_weight,
+            "ssim_weight": initial_ssim_weight,
+            "recon_decay_rate": recon_decay_rate,
+            "vgg_decay_rate": vgg_decay_rate,
+            "kl_beta_decay_rate": kl_beta_decay_rate,
+            "ssim_decay_rate": ssim_decay_rate,
+            # "recon_weight": self.recon_weight,
+            # "kl_beta": self.kl_beta,
+            # "vgg_weight": self.vgg_weight
         }
         
         return custom_configs
@@ -257,17 +300,28 @@ class DynamicVAE(keras.Model):
         input_shape = config["build_input_shape"]
         model_optimizer = config["optimizer_fn"]
         learning_rate = config["learning_rate"]
-        alpha = config["alpha"]
+        recon_weight = config["recon_weight"]
         kl_beta = config["kl_beta"]
-        gamma = config["gamma"]
+        vgg_weight = config["vgg_weight"]
+        ssim_weight = config["ssim_weight"]
+
+        recon_decay_rate = 1.0,
+        vgg_decay_rate = 0.95,
+        kl_beta_decay_rate = 0.95,
+        ssim_decay_rate= 0.95,
         #rebuild model
         model = DynamicVAE(model_name=model_name, 
                            latent_dims=latent_dims, 
                            optimizer_fn=model_optimizer, 
                            learning_rate=learning_rate,
-                           alpha=alpha,
-                           gamma = gamma,
-                           kl_beta=kl_beta
+                           recon_weight=recon_weight,
+                           vgg_weight = vgg_weight,
+                           kl_beta=kl_beta,
+                           ssim_weight=ssim_weight,
+                           recon_decay_rate = recon_decay_rate,
+                           vgg_decay_rate = vgg_decay_rate,
+                           kl_beta_decay_rate = kl_beta_decay_rate,
+                           ssim_decay_rate= ssim_decay_rate,
                            )
         model.build(input_shape)
         
@@ -307,3 +361,47 @@ class DynamicVAE(keras.Model):
         return True
 
 
+
+#load data
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype("float32")/255.0
+x_test = x_test.astype("float32")/255.0
+
+
+
+
+
+learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
+            initial_learning_rate=0.0001, 
+            decay_steps=5000, 
+            decay_rate=0.99, 
+            staircase=True
+        )
+
+
+optimizer = keras.optimizers.Adam
+model_name = "schedule_vae"
+latent_dims = 32
+vae = DynamicVAE(model_name=model_name,
+                 optimizer_fn=optimizer,
+                 learning_rate=learning_rate,
+                 latent_dims=latent_dims
+)
+
+
+callbacks = [
+    keras.callbacks.LambdaCallback(on_epoch_end=lambda epoch, logs: vae.scheduler_callback(epoch, logs))
+]
+
+
+
+epochs=100
+batch_size=64
+
+
+vae.fit(x_train,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, x_test),
+        callbacks=callbacks
+)
