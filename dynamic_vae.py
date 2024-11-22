e24ac9ca11125935a06f732fff2ee13a4dcce4d1
maman510 <153794692+maman510@users.noreply.github.com>
2024-11-21 22:24:48 -0800

    Add files via upload

--- dynamic_vae.py
+++ dynamic_vae.py
@@ -4,11 +4,14 @@ import keras.applications.vgg16
 import keras.layers
 import keras.losses
 import keras.optimizers
+import keras.utils
 import tensorflow as tf
 import numpy as np
 import time
 from keras.datasets import cifar10
 import matplotlib.pyplot as plt
+import tool_box
+import os
 
 tf.random.set_seed(42)
 np.random.seed(42)
@@ -16,22 +19,52 @@ tf.config.run_functions_eagerly(True)
 
 class DynamicVAE(keras.Model):
 
-    def __init__(self, encoder, decoder, training_epoch_count, kl_beta_initial=1.0):
+    def __init__(self, model_name, latent_dims, optimizer_fn, learning_rate):
         super(DynamicVAE, self).__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self.kl_beta = kl_beta_initial
-        
-        #=== vgg and ssim
+        self.model_name = model_name
+        self.latent_dims = latent_dims
+
+        #NOTE: PASS UNINVOKED OPTIMIZER (e.g. pass: keras.optimizers.Nadam NOT: keras.optimizers.Nadam() - optimizer invoked in build)
+        self.optimizer_fn = optimizer_fn
+        self.learning_rate = learning_rate
+
+        self.compile(optimizer=self.optimizer_fn(self.learning_rate))
+
+
+    def build(self, input_shape):
+        self.build_input_shape = input_shape
+        self.encoder = self.build_encoder(input_shape[1:], self.latent_dims)
+        self.decoder = self.build_decoder(self.latent_dims, input_shape[1:])
         self.vgg_model = self._build_vgg_model()
 
-        
-        
-        #define total epochs for training
-        self.training_epoch_count = training_epoch_count
-        self.current_epoch = 0
-        self.current_recon_level = None
-        
+        self.optimizer = self.optimizer_fn(self.learning_rate)
+
+        return super(DynamicVAE, self).build(input_shape)
+    
+    def build_encoder(self, input_shape, latent_dim):
+        inputs = keras.Input(shape=input_shape)
+        x = keras.layers.Conv2D(32, kernel_size=(3,3), activation="relu", strides=2, padding="same")(inputs)
+        x = keras.layers.Conv2D(64, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
+        x = keras.layers.Conv2D(128, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
+        #start bottleneck
+        x = keras.layers.Flatten()(x)
+        x = keras.layers.Dense(128, activation="relu")(x)
+        z_mean = keras.layers.Dense(latent_dim)(x)
+        z_log_var = keras.layers.Dense(latent_dim)(x)
+        return keras.Model(inputs, [z_mean, z_log_var], name="encoder")
+    
+    def build_decoder(self, latent_dim, original_shape):
+        reshape_layer_shape = (original_shape[0]//4, original_shape[1]//4,64)
+        latent_inputs = keras.Input(shape=(latent_dim,))
+        x = keras.layers.Dense(128, activation="relu")(latent_inputs)
+        x = keras.layers.Dense(np.prod(reshape_layer_shape), activation="relu")(x)
+        x = keras.layers.Reshape(reshape_layer_shape)(x)
+        #begin upsampling
+        x = keras.layers.Conv2DTranspose(64, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
+        x = keras.layers.Conv2DTranspose(32, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
+        outputs = keras.layers.Conv2DTranspose(original_shape[2], kernel_size=(3,3), activation="sigmoid", padding="same")(x)
+        return keras.Model(latent_inputs, outputs, name="decoder")
+
     def _build_vgg_model(self):
         vgg = keras.applications.VGG16(weights="imagenet", include_top=False)
       #  'block1_conv2', 'block2_conv2' ,'block5_conv2', 'block5_conv3'
@@ -67,6 +100,7 @@ class DynamicVAE(keras.Model):
         
         return tf.reduce_mean(kl_div)
     
+
     def _compute_reconstruction_error(self, input_images, reconstructed, delta=1.0):
         huber_loss = tf.keras.losses.Huber(delta=delta)
         return huber_loss(input_images, reconstructed)
@@ -88,6 +122,7 @@ class DynamicVAE(keras.Model):
         return kl_loss, reconstruction_loss, perceptual_loss, ssim_loss, total_loss
     
     def call(self, inputs):
+    
         z_mean, z_log_var = self.encoder(inputs)
         z = self._sampling(z_mean, z_log_var)
         reconstructed = self.decoder(z)
@@ -96,7 +131,7 @@ class DynamicVAE(keras.Model):
     
  
     def train_step(self, original_images):
-        self.current_epoch += 1
+   
         with tf.GradientTape() as tape:
             #forward pass:
             reconstructed, z_mean, z_log_var = self(original_images)
@@ -117,39 +152,6 @@ class DynamicVAE(keras.Model):
         return {"loss": total_loss, "vgg_loss": perceptual_loss , "ssim_loss": ssim_loss, "reconstruction_error": reconstruction_loss, "kl_divergence": kl_divergence}
     
 
-    def _check_update(self, recon_error):
-      
-        if recon_error < 0.2:
-            current_recon_level = "very_low"
-        elif recon_error >= 0.2 and recon_error < 0.4:
-            current_recon_level = "low"
-        elif recon_error >= 0.4 and recon_error < 0.6:
-            current_recon_level = "medium_low"
-        elif recon_error >= 0.6 and recon_error < 0.8:
-            current_recon_level = "medium"
-        else:
-            current_recon_level = "high"
-        return current_recon_level
-
-
-
-    def adjust_kl_beta(self, reconstruction_error, kl_divergence):
-        #find what stage of training loop is
-        recon_level = self._check_update(reconstruction_error)
-        if self.current_recon_level != recon_level:
-            print(f"\n\nUPDATING RECON LEVEL...\n\n")
-            time.sleep(2)
-            self.current_recon_level = recon_level
-
-        beta_schedule = {
-                    "high": 0.1,          
-                    "medium": 0.2,
-                    "medium_low": 0.4,
-                    "low": 0.6,     
-                    "very_low": 1.0              
-                }
-        
-        return beta_schedule[self.current_recon_level]
     
     def reconstruct_image(self, original_image):
         # Reconstruct the image using the VAE
@@ -172,68 +174,83 @@ class DynamicVAE(keras.Model):
         axes[1].axis("off")
 
         plt.show()
-                
 
+    def save(self, overwrite=False):
+        file_path = f"{os.getcwd()}/trained_models/{self.model_name}.pkl"
+        if os.path.exists(file_path) == True and overwrite == False:
+            response = input(tool_box.color_string('red', f'\n\nMODEL CONFIGS FOUND @ PATH: {file_path}; OVERWRITE EXISTING MODEL? ("y" or "n")\n\n'))
+            if response.lower() == 'n':
+                return None
+            elif response.lower() == 'y':
+                print(tool_box.color_string('green', f"\nOVERWRITING EXISTING FILE....\n"))
+                return self.save(overwrite=True)
+            else:
+                print(tool_box.color_string('yellow', f"\nINVALID RESPONSE; USE 'n' or 'y'\n"))
+                return self.save(overwrite=False)
+        else:
+            configs = self.get_config()
+            tool_box.Create_Pkl(file_path, configs)
+            print(tool_box.color_string('green', f'\n\nSAVED MODEL CONFIGS @ PATH: {file_path}\n\n'))
+            return configs
+        
+    @classmethod
+    def load_model(cls, model_name):
+        file_path = f"{os.getcwd()}/trained_models/{model_name}.pkl"
+        if os.path.exists(file_path) == False:
+            print(tool_box.color_string('red', f'\n\nNO MODEL CONFIGS FOUND @ PATH: {file_path}\n\n'))
+            return None
+        else:
+            configs = tool_box.Load_Pkl(file_path)
+            model = DynamicVAE.from_config(configs)
+            print(tool_box.color_string('green', f'\n\nRETURNING MODEL BUILT WITH CONFIGS FOUND @ PATH: {file_path}\n\n'))
+            return model
 
+    def get_config(self):
+   
+        custom_configs = {
+            "model_name": self.model_name,
+            "latent_dims": self.latent_dims,
+            "encoder": self.encoder,
+            "decoder": self.decoder,
+            "vgg_model": self.vgg_model,
+            "build_input_shape": self.build_input_shape,
+            "optimizer_fn": self.optimizer_fn,
+            "learning_rate": self.learning_rate,
+            "weights": self.weights
+        }
+        
+        return custom_configs
+    
+    @classmethod
+    def from_config(cls, config):
+        #extract modeling init params
+        model_name = config["model_name"]
+        latent_dims = config["latent_dims"]
+        input_shape = config["build_input_shape"]
+        model_optimizer = config["optimizer_fn"]
+        learning_rate = config["learning_rate"]
 
+        #rebuild model
+        model = DynamicVAE(model_name=model_name, latent_dims=latent_dims, optimizer_fn=model_optimizer, learning_rate=learning_rate)
+        model.build(input_shape)
+        
+        #load model weights and set
+        weights = config["weights"]
+        model.set_weights(weights)
 
-def build_encoder(input_shape, latent_dim):
-    inputs = keras.Input(shape=input_shape)
-    x = keras.layers.Conv2D(32, kernel_size=(3,3), activation="relu", strides=2, padding="same")(inputs)
-    x = keras.layers.Conv2D(64, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
-    x = keras.layers.Conv2D(128, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
-    #start bottleneck
-    x = keras.layers.Flatten()(x)
-    x = keras.layers.Dense(128, activation="relu")(x)
-    z_mean = keras.layers.Dense(latent_dim)(x)
-    z_log_var = keras.layers.Dense(latent_dim)(x)
+        return model
+    
+    @classmethod
+    # Function to compare weights
+    def compare_model_weights(cls,weights_1, weights_2):
+        if len(weights_1) != len(weights_2):
+            print("The models have different numbers of layers or weights.")
+            return False
+        
+        for i in range(len(weights_1)):
+            if not np.array_equal(weights_1[i], weights_2[i]):
+                print(f"Layer {i} weights are different.")
+                return False
 
-    return keras.Model(inputs, [z_mean, z_log_var], name="encoder")
-
-
-def build_decoder(latent_dim, original_shape):
-    reshape_layer_shape = (original_shape[0]//4, original_shape[1]//4,64)
-    latent_inputs = keras.Input(shape=(latent_dim,))
-    x = keras.layers.Dense(128, activation="relu")(latent_inputs)
-    x = keras.layers.Dense(np.prod(reshape_layer_shape), activation="relu")(x)
-    x = keras.layers.Reshape(reshape_layer_shape)(x)
-    #begin upsampling
-    x = keras.layers.Conv2DTranspose(64, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
-    x = keras.layers.Conv2DTranspose(32, kernel_size=(3,3), activation="relu", strides=2, padding="same")(x)
-    outputs = keras.layers.Conv2DTranspose(original_shape[2], kernel_size=(3,3), activation="sigmoid", padding="same")(x)
-    return keras.Model(latent_inputs, outputs, name="decoder")
-
-
-
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-x_train = x_train.astype("float32")/255.0
-x_test = x_test.astype("float32")/255.0
-
-input_shape = (32,32,3)
-
-latent_dim = 32
-
-encoder = build_encoder(input_shape, latent_dim)
-decoder = build_decoder(latent_dim, input_shape)
-
-epochs=100
-
-vae = DynamicVAE(encoder, decoder, training_epoch_count=epochs)
-lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate=1e-3, 
-    decay_steps=100000, 
-    decay_rate=0.96, 
-    staircase=True
-)
-
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
-
-vae.compile(optimizer=optimizer)
-
-
-vae.fit(x_train,  epochs=epochs, batch_size=32)
-
-# Testing with one image
-original_image = x_test[0:1]  # Take the first image from the test set
-
-vae.reconstruct_image(original_image)
+        print("The weights of both models are identical.")
+        return True
